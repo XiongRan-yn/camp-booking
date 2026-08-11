75d62604bee7def8a698cf8d03c22b9b87e26516
BAEQXJ0716 <baeqxj0716@outlook.com>
2026-08-11 14:55:22 +0800

    fix(vant): 全局注册Vant组件，修复页面空白

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -7,8 +7,11 @@ import App from "./App.vue";
 import "./styles/reset.css";
 import "./styles/variables.css";
 import "./styles/common.css";
+import Vant from "vant";
+import "vant/lib/index.css";
 
 const app = createApp(App);
 app.use(createPinia());
 app.use(router);
+app.use(Vant);
 app.mount("#app");
